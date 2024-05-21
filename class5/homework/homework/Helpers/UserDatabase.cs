e3cd6d786e0c9036b663275edb386fa9cbeb9515
Milica <dujakovic.milica3@gmail.com>
2024-05-21 13:10:12 +0200

    homework done

---
+++ class5/homework/homework/Helpers/UserDatabase.cs
@@ -0,0 +1,84 @@
+﻿using homework.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace homework.Helpers
+{
+    public static class UserDatabase
+    {
+       public static List<User> Users { get; set; }
+        public static void GetUserById(int id)
+        {
+           List<User> userById = Users.Where(x => x.Id == id).ToList();
+            if(userById.Count == 0) {
+                Console.WriteLine($"User by Id {id} not found.");
+            }else if (userById.Count == 1)
+            {
+                Console.WriteLine($"User by Id {id} found. Name: {userById[0].Name}, Age:{userById[0].Age}.");
+            }
+            else
+            {
+                Console.WriteLine("Error. Try again.");
+            }
+
+        }
+        public static void GetUserByName(string name)
+        {
+            List<User> userByName = Users.Where(x => x.Name.ToLower() == name).ToList();
+            if (userByName.Count == 0)
+            {
+                Console.WriteLine($"User by Name {name} not found");
+            }
+            else if (userByName.Count == 1)
+            {
+                Console.WriteLine($"User by Name {name} found. Name: {userByName[0].Name}, Age:{userByName[0].Age}.");
+            }
+            else if (userByName.Count > 1)
+            {
+                Console.WriteLine("There is more user with that name. Users are: ");
+                foreach (User item in userByName)
+                {
+                    Console.WriteLine($"User Id:{item.Id}, Name: {item.Name}, Age:{item.Age}");
+                }
+               
+            }
+            else
+            {
+                Console.WriteLine("Error. Try again.");
+            }
+        }
+        public static void GetUserByAge(int age)
+        {
+            List<User> userByAge = Users.Where(x => x.Age == age).ToList();
+            if (userByAge.Count == 0)
+            {
+                Console.WriteLine($"User by Age {age} not found.");
+            }
+            else if (userByAge.Count == 1)
+            {
+                Console.WriteLine($"User by Age {age} found. Name: {userByAge[0].Name}, Age:{userByAge[0].Age}.");
+            }
+            else if (userByAge.Count > 1)
+            {
+
+                Console.WriteLine("There is more users with that age. Users are: ");
+                foreach (User item in userByAge)
+                {
+                    Console.WriteLine($"User Id:{item.Id}, Name: {item.Name}, Age:{item.Age}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error. Try again.");
+            }
+
+        }
+       
+    }
+   
+}
---
+++ class5/homework/homework/Program.cs
@@ -0,0 +1,55 @@
+﻿using homework.Helpers;
+using homework.Models;
+using System.Security.Cryptography.X509Certificates;
+
+namespace homework
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<User> users = new List<User>
+            {
+                new User(1, "Sava", 22),
+                new User(2, "Tamara", 25),
+                new User(3, "Dordje", 28),
+                new User(4, "Goran", 27),
+                new User(5, "Dusan", 30),
+                new User(6, "Almir", 29),
+                new User(7, "Goran", 23),
+                new User(8, "Stefan",23 ),
+            };
+            UserDatabase.Users = users;
+
+            Console.WriteLine("Search user by : ");
+            Console.WriteLine("1.Id");
+            Console.WriteLine("2.Name");
+            Console.WriteLine("3.Age ");
+            int userInput = int.Parse(Console.ReadLine());  
+
+            switch (userInput)
+            {
+                case 1:
+                    Console.WriteLine("Enter Id:");
+                    int idInput = int.Parse(Console.ReadLine());
+                    UserDatabase.GetUserById(idInput);
+                    break;
+                case 2:
+                    Console.WriteLine("Enter Name:");
+                    string nameInput = Console.ReadLine().ToLower();
+                    UserDatabase.GetUserByName(nameInput);
+                    break;
+                case 3:
+                    Console.WriteLine("Enter Age:");
+                    int ageInput =int.Parse(Console.ReadLine());
+                    UserDatabase.GetUserByAge(ageInput);
+                    break;
+                default:
+                    Console.WriteLine("Input unknown.Try again.");
+                    break;
+            }
+            
+
+        }
+    }
+}
