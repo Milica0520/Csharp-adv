19d73d541e57f53e2a15e5ccb4b3b765570ae345
Milica <dujakovic.milica3@gmail.com>
2024-05-22 13:10:19 +0200

    homework task1 done

---
+++ class6/homework/homework/Models/PrintInConsole.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework.Models
+{
+    public class PrintInConsole<T>
+    {
+        public List<T> Collection = new List<T>();
+        public void Print(T obj) { 
+            Console.WriteLine(obj); 
+        }
+
+        public void PrintCollection()
+        {
+
+            foreach (T item in Collection)
+            {
+                Print(item);
+            }
+
+
+        }
+    }
+}
---
+++ class6/homework/homework/Program.cs
@@ -0,0 +1,24 @@
+﻿using homework.Models;
+
+namespace homework
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintInConsole<string> words = new PrintInConsole<string>();
+
+            words.Collection.Add("This");
+            words.Collection.Add("is");
+            words.Collection.Add("simple");
+            words.Collection.Add("sentence.");
+
+
+            words.PrintCollection();
+
+            words.Print("This");
+
+            
+        }
+    }
+}
