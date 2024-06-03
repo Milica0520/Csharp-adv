77b0fd5805b89005cb057c5b914944180510bb96
Milica <dujakovic.milica3@gmail.com>
2024-06-03 21:08:11 +0200

    code added

--- class9/class9/obj/Release/net8.0/class9.AssemblyInfo.cs
+++ class9/class9/obj/Release/net8.0/class9.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("class9")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2688bc299d7ba6be15019f043187789af47d2dec")]
 [assembly: System.Reflection.AssemblyProductAttribute("class9")]
